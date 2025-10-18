f45fe1e860ac2908a77e2f36fe89d369470b9ac6
tuanhungstar <58647622+tuanhungstar@users.noreply.github.com>
2025-10-19 00:32:44 +0200

    Update Mouse_Key_Automate.py

--- Bot_module/Mouse_Key_Automate.py
+++ Bot_module/Mouse_Key_Automate.py
@@ -1,8 +1,11 @@
 import keyboard
-
+import pyautogui
+from my_lib.shared_context import ExecutionContext as Context
+import Gui_Automate
+import Mouse_Key_Automate
 class Keyboard:
-    def __init__(self):
-
+    def __init__(self,context: Context):
+        self.context = Context
         pass
     def send_text(self,text):
         keyboard.write(str(text))
@@ -14,3 +17,25 @@ class Keyboard:
         """
         keyboard.press_and_release(hotkey)
         return f"Bot send this hotkey: {hotkey}"
+        
+
+    def moveMouse(self,x=50,y=50):
+        ''' 
+
+
+        move mouse to deserd position
+        (0,0)
+          +------------> X-Axis
+          |
+          |
+          |
+          |
+          |
+          |
+          |
+          v
+         Y-Axis
+        '''
+        pyautogui.moveTo(x,y)
+        pass
+        return
